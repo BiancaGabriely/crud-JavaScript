ddddd072028cb690668a22fa4621f9be925d544b
BiancaGabriely <bianca.gabriely@escolar.ifrn.edu.br>
2026-05-20 14:43:30 -0300

    Primeira versão do CRUD

---
+++ script.js
@@ -0,0 +1,48 @@
+let usuarios = [];
+
+function cadastrar(){
+    let nome = document.getElementById("nome").value;
+    let cpf = Number(document.getElementById("cpf").value);
+
+    let usuario = {
+        cpf: cpf,
+        nome: nome
+    };
+
+    let existe = usuarios.some(u => u.cpf === cpf);
+
+    if(existe){
+        alert("ERRO: Este CPF já está cadastrado.");
+        return;
+    }
+
+    usuarios.push(usuario);
+    alert("Usuário Cadastrado com Sucesso!");
+
+    console.log(usuarios);
+
+    document.getElementById("nome").value = "";
+    document.getElementById("cpf").value = "";
+
+    // Coloca o cursor de volta no campo Nome para o próximo cadastro
+    document.getElementById("nome").focus();
+
+};
+
+function listar(){
+    let lista = document.getElementById("lista");
+    let tabela = document.getElementById("tusuarios");
+
+    lista.innerHTML = "";
+
+    usuarios.forEach(usuario => {
+        lista.innerHTML += `
+        <tr>
+            <td>${usuario.cpf}</td>
+            <td>${usuario.nome}</td>
+        </tr>
+        `;
+    });
+
+    tabela.style.display = "table";
+};
